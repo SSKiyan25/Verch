7774bf55f65ceddf3ed7f9cd25b0be57c6952d58
Joshua A. Sosmeña <126632428+SSKiyan25@users.noreply.github.com>
2024-08-07 23:38:58 +0800

    just a start

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import {
+    createRouter,
+    createWebHistory,
+    RouteRecordRaw,
+  } from "vue-router";
+import Home from "@/components/user/home/views/Home.vue";  
+
+  const routes: RouteRecordRaw[] = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+  ];
+
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+
+  export default router;
